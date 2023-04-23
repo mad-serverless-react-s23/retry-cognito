7a74176401815d7be3973363e585c36d841cf8ad
MysticalSkeptic <115025255+MysticalSkeptic@users.noreply.github.com>
2023-04-22 22:23:00 -0500

    Update Public.js
    built as book with my text for h1

---
+++ src/Public.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Container from './Container';
+function Public() {
+    return (
+        <Container>
+            <h1>This is a public space. Like it?</h1>
+        </Container>
+    )
+}
+export default Public
